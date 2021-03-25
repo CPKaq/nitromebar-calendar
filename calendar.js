780f3c8c3fc24262edbad5f3ce38ca88adbfccb4
CPK <CPKbaidu@163.com>
2021-03-25 20:55:42 +0800

    add month

--- calendar.js
+++ calendar.js
@@ -1,9 +1,11 @@
-var today = new Date();
-var startDate = new Date(2020, 10, 30); //2020年11月30日
+const today = new Date();
+const startDate = new Date(2020, 10, 30); //2020年11月30日
 
 var outBox = document.getElementById("data");
+var monthBox = document.getElementById("month");
+var d = new Date();
 
-var d = startDate;
+d.setTime(startDate.getTime());
 var index = 0;
 while(d<=today)
 {
@@ -18,4 +20,21 @@ while(d<=today)
     para.appendChild(node);
     outBox.appendChild(para);
     d.setDate(d.getDate()+1);
+}
+
+
+d.setTime(startDate.getTime());
+while(d<=today)
+{
+    var para = document.createElement("div");
+    var node = document.createTextNode("");
+    para.className = "monthbox";
+    if(d.getDate()<=7)
+    {
+        node = document.createTextNode((d.getMonth()+1)+"月");
+    }
+    para.appendChild(node);
+    monthBox.appendChild(para);
+
+    d.setDate(d.getDate()+7);
 }
