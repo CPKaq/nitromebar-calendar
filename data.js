d957fe7e03b9226b0179ee08b27313c26f9b2b8c
CPK <CPKbaidu@163.com>
2021-04-03 13:17:34 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var submitDate = [
     {date: new Date(2020, 11, 18), bgColor: "#9121AF", fontColor: "#F64403", index: 1, name: "团团"},
     {date: new Date(2020, 11, 22), bgColor: "#0B2174", fontColor: "#C5BCB5", index: 2, name: "米糕"},
     {date: new Date(2020, 11, 31), bgColor: "#BBDBDA", fontColor: "#D27DB8", index: 3, name: "nitromeboll"},
-    {date: new Date(2021,  0,  9), bgColor: "#FFFFFF", fontColor: "#000000", index: 4, name: "CPK"},
+    {date: new Date(2021,  0,  9), bgColor: "#026632", fontColor: "#029661", index: 4, name: "CPK"},
     {date: new Date(2021,  0, 25), bgColor: "#F2F2F2", fontColor: "#999999", index: -1},
     {date: new Date(2021,  1, 24), bgColor: "#FDFEF9", fontColor: "#25497D", index: 5, name: "云梦旅"},
     {date: new Date(2021,  1, 25), bgColor: "#94C7C0", fontColor: "#3391C7", index: 6, name: "窥屏坑"},
